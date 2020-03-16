a384964a7c10e7e3d6b5a44d4b7188c182dfb8a0
Pedro Ferracini <pedro.ferracini@imusics.com>
2020-03-16 13:57:23 -0300

    Gitignore file, renaming tensorflow LSTM file and basic main with parse_arguments

---
+++ src/tf/LSTM-tf-Google.py
@@ -0,0 +1,30 @@
+import argparse
+from collections import OrderedDict
+
+global ARGS
+global tPARAMS
+
+
+def parse_arguments():
+  parser = argparse.ArgumentParser()
+  parser.add_argument('inputFileRadical', type=str, metavar='<visit_file>', help='File radical name (the software will look for .train and .test files) with pickled data organized as patient x admission x codes.')
+  parser.add_argument('outFile', metavar='out_file', default='model_output', help='Any file name to store the model.')
+  parser.add_argument('--maxConsecutiveNonImprovements', type=int, default=5, help='Training wiil run until reaching the maximum number of epochs without improvement before stopping the training')
+  parser.add_argument('--hiddenDimSize', type=str, default='[271]', help='Number of layers and their size - for example [100,200] refers to two layers with 100 and 200 nodes.')
+  parser.add_argument('--batchSize', type=int, default=100, help='Batch size.')
+  parser.add_argument('--nEpochs', type=int, default=1000, help='Number of training iterations.')
+  parser.add_argument('--LregularizationAlpha', type=float, default=0.001, help='Alpha regularization for L2 normalization')
+  parser.add_argument('--dropoutRate', type=float, default=0.45, help='Dropout probability.')
+
+  ARGStemp = parser.parse_args()
+  hiddenDimSize = [int(strDim) for strDim in ARGStemp.hiddenDimSize[1:-1].split(',')]
+  ARGStemp.hiddenDimSize = hiddenDimSize
+  return ARGStemp
+
+if __name__ == '__main__':
+  global tPARAMS
+  tPARAMS = OrderedDict()
+  global ARGS
+  ARGS = parse_arguments()
+
+  # train_model()
