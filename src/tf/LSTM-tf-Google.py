134e3b7623bb7cabf790859af5b1523d2c8ce01c
Pedro Ferracini <pedro.ferracini@imusics.com>
2020-03-28 17:23:23 -0300

    Implements prepareHotVectors function as theano file and begins the training process

--- src/tf/LSTM-tf-Google.py
+++ src/tf/LSTM-tf-Google.py
@@ -1,7 +1,9 @@
+import random
 import pickle
 import argparse
 from collections import OrderedDict
 import tensorflow as tf
+import numpy as np
 
 global ARGS
 
@@ -15,15 +17,40 @@ def getNumberOfCodes(sets):
             highestCode = code
   return (highestCode + 1)
 
+def prepareHotVectors(train_tensor, labels_tensor):
+  nVisitsOfEachPatient_List = np.array([len(seq) for seq in train_tensor]) - 1
+  numberOfPatients = len(train_tensor)
+  maxNumberOfAdmissions = np.max(nVisitsOfEachPatient_List)
+
+  x_hotvectors_tensorf = np.zeros((maxNumberOfAdmissions, numberOfPatients, ARGS.numberOfInputCodes)).astype(np.float64)
+  y_hotvectors_tensor = np.zeros((maxNumberOfAdmissions, numberOfPatients, ARGS.numberOfInputCodes)).astype(np.float64)
+  mask = np.zeros((maxNumberOfAdmissions, numberOfPatients)).astype(np.float64)
+
+  for idx, (train_patient_matrix,label_patient_matrix) in enumerate(zip(train_tensor,labels_tensor)):
+    for i_th_visit, visit_line in enumerate(train_patient_matrix[:-1]): #ignores the last admission, which is not part of the training
+      for code in visit_line:
+        x_hotvectors_tensorf[i_th_visit, idx, code] = 1
+    for i_th_visit, visit_line in enumerate(label_patient_matrix[1:]):  #label_matrix[1:] = all but the first admission slice, not used to evaluate (this is the answer)
+      for code in visit_line:
+        y_hotvectors_tensor[i_th_visit, idx, code] = 1
+    mask[:nVisitsOfEachPatient_List[idx], idx] = 1.
+
+  nVisitsOfEachPatient_List = np.array(nVisitsOfEachPatient_List, dtype=np.float64)
+  return x_hotvectors_tensorf, y_hotvectors_tensor, mask, nVisitsOfEachPatient_List
+
 def LSTM_cell(layerSize):
   cell = tf.keras.layers.LSTMCell(layerSize)
   return cell
 
 def build_model(lstmCells):
-  inputs = tf.keras.Input((1, ARGS.numberOfInputCodes))
+  inputs = tf.keras.Input((ARGS.batchSize, ARGS.numberOfInputCodes))
+
+  # numero variavel de tamanhos de inputs
+  # ver arquitetura pronta de theano rnn
+
   x = tf.keras.layers.RNN(lstmCells)(inputs)
   model = tf.keras.models.Model(inputs, x)
-  print(model.summary())
+  # print(model.summary())
   model.compile(optimizer='Adadelta', loss='mse')
   return model
 
@@ -64,6 +91,27 @@ def train_model():
   print('==> model building')
   model = build_model(lstmCells)
 
+  print('==> training and validation')
+  batchSize = ARGS.batchSize
+  n_batches = int(np.ceil(float(len(trainSet[0])) / float(batchSize)))
+  # TEST_MODEL_COMPILED = theano.function(inputs=[xf, y, mask, nVisitsOfEachPatient_List], outputs=MODEL, name='TEST_MODEL_COMPILED')
+
+  bestValidationCrossEntropy = 1e20
+  bestValidationEpoch = 0
+  bestModelFileName = ''
+
+  iImprovementEpochs = 0
+  iConsecutiveNonImprovements = 0
+  epoch_counter = 0
+  for epoch_counter in range(ARGS.nEpochs):
+    iteration = 0
+    trainCrossEntropyVector = []
+    for index in random.sample(range(n_batches), n_batches):
+      batchX = trainSet[0][index*batchSize:(index+1)*batchSize]
+      batchY = trainSet[1][index*batchSize:(index+1)*batchSize]
+      xf, y, mask, nVisitsOfEachPatient_List = prepareHotVectors(batchX, batchY)
+      xf += np.random.normal(0, 0.1, xf.shape)  #add gaussian noise as a means to reduce overfitting
+      print(np.shape(xf), np.shape(y), np.shape(mask), np.shape(nVisitsOfEachPatient_List))
 
 def parse_arguments():
   parser = argparse.ArgumentParser()
