7dbdfa06f003f02786eef793e8ec870c4e923168
Pedro Ferracini <pedro.ferracini@imusics.com>
2020-04-13 15:36:19 -0300

    Changes on train script

--- train-tensorflow.sh
+++ train-tensorflow.sh
@@ -1,5 +1,1 @@
-python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model --nEpochs=50
-python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model --nEpochs=100
-python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model --nEpochs=200
-python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model --nEpochs=500
-python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model
+python3 src/tf/LSTM-tf-Google.py "data/_846" compiled_models/tensorflow-model --maxConsecutiveNonImprovements=10
