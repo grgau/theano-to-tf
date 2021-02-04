57e8b4b2777666a95f173b75cdc6d9ce69a9c7e4
Pedro Ferracini <pedro.ferracini@imusics.com>
2021-02-04 19:43:19 -0300

    WIP: Encoder decoder

--- src/tf/LSTM-tf-Google.py
+++ src/tf/LSTM-tf-Google.py
@@ -72,7 +72,7 @@ def load_data():
 
   return trainSet, testSet
 
-def performEvaluation(session, loss, x, y, mask, seqLen, training, test_Set):
+def performEvaluation(session, loss, x, y, mask, seqLen, test_Set):
   batchSize = ARGS.batchSize
 
   n_batches = int(np.ceil(float(len(test_Set[0])) / float(batchSize))) #default batch size is 100
@@ -92,7 +92,7 @@ def performEvaluation(session, loss, x, y, mask, seqLen, training, test_Set):
       # end_token = np.full((yf.shape[0], yf.shape[1], 1), 200)
       # yf = np.concatenate([yf, end_token], axis=-1)
 
-      feed_dict = {x: xf, y: yf, mask: maskf, seqLen: nVisitsOfEachPatient_List, training: True}
+      feed_dict = {x: xf, y: yf, mask: maskf, seqLen: nVisitsOfEachPatient_List}
       crossEntropy = sess.run(loss, feed_dict=feed_dict)
 
       #accumulation by simple summation taking the batch size into account
@@ -101,7 +101,7 @@ def performEvaluation(session, loss, x, y, mask, seqLen, training, test_Set):
       #At the end, it returns the mean cross entropy considering all the batches
   return n_batches, crossEntropySum / dataCount
 
-def EncoderDecoderBahdanau_layer(inputTensor, targetTensor, seqLen):
+def EncoderDecoderBahdanau_layer(inputTensor, targetTensor, vocab, seqLen):
   # Encoder
   with tf.variable_scope('encoder_cell'):
     lstms = [tf.nn.rnn_cell.BasicLSTMCell(size, state_is_tuple=True) for size in ARGS.hiddenDimSize] #According to docs (https://www.tensorflow.org/api_docs/python/tf/compat/v1/nn/rnn_cell/LSTMCell), the peephole version is based on LSTM Google (2014)
@@ -112,57 +112,51 @@ def EncoderDecoderBahdanau_layer(inputTensor, targetTensor, seqLen):
   with tf.variable_scope('decoder_cell'):
     seqLen = tf.cast(seqLen, dtype=tf.int32)
     # Bahdanau Attention
-    query_with_time_axis = tf.expand_dims(lstm_states[-1].h, axis=1)
-    score = tf.keras.layers.Dense(1)(
-      tf.nn.tanh(
-        tf.keras.layers.Dense(ARGS.attentionDimSize)(query_with_time_axis) + \
-        tf.keras.layers.Dense(ARGS.attentionDimSize)(tf.transpose(lstm_outputs, [1, 0, 2]))
-      )
-    )
-    attention_weights = tf.nn.softmax(score, axis=1)
-    context_vector = attention_weights * tf.transpose(lstm_outputs, [1, 0, 2])
-    context_vector = tf.reduce_sum(context_vector, axis=1)
-
-    context_vector = tf.expand_dims(context_vector, axis=0)
-    context_vector = tf.tile(context_vector, multiples=[tf.shape(lstm_outputs)[0], 1, 1])
-    lstm_outputs = tf.concat([context_vector, lstm_outputs], axis=-1)
+    # query_with_time_axis = tf.expand_dims(lstm_states[-1].h, axis=1)
+    # score = tf.keras.layers.Dense(1)(
+    #   tf.nn.tanh(
+    #     tf.keras.layers.Dense(ARGS.attentionDimSize)(query_with_time_axis) + \
+    #     tf.keras.layers.Dense(ARGS.attentionDimSize)(tf.transpose(lstm_outputs, [1, 0, 2]))
+    #   )
+    # )
+    # attention_weights = tf.nn.softmax(score, axis=1)
+    # context_vector = attention_weights * tf.transpose(lstm_outputs, [1, 0, 2])
+    # context_vector = tf.reduce_sum(context_vector, axis=1)
+    #
+    # context_vector = tf.expand_dims(context_vector, axis=0)
+    # context_vector = tf.tile(context_vector, multiples=[tf.shape(lstm_outputs)[0], 1, 1])
+    # lstm_outputs = tf.concat([context_vector, lstm_outputs], axis=-1)
 
     # Decoder
     lstms = [tf.nn.rnn_cell.BasicLSTMCell(size, state_is_tuple=True) for size in ARGS.hiddenDimSize] #According to docs (https://www.tensorflow.org/api_docs/python/tf/compat/v1/nn/rnn_cell/LSTMCell), the peephole version is based on LSTM Google (2014)
-    # lstms = [tf.nn.rnn_cell.DropoutWrapper(lstm, state_keep_prob=ARGS.dropoutRate) for lstm in lstms]
-    cell = tf.nn.rnn_cell.MultiRNNCell(lstms)
+    lstms = [tf.nn.rnn_cell.DropoutWrapper(lstm, state_keep_prob=ARGS.dropoutRate) for lstm in lstms]
+    dec_cell = tf.nn.rnn_cell.MultiRNNCell(lstms)
 
     sampler = tfa.seq2seq.sampler.TrainingSampler(time_major=True)
-    # sampler.initialize(targetTensor, sequence_length=seqLen)
-    decoder = tfa.seq2seq.BasicDecoder(cell, sampler=sampler)
-    # decoder.initialize(targetTensor, initial_state=lstm_states)
+    decoder = tfa.seq2seq.BasicDecoder(dec_cell, sampler=sampler)
 
-    # else:
-    #   # Inference
-    #   inference_sampler = tfa.seq2seq.GreedyEmbeddingSampler()
-    #   inference_sampler.initialize(inputTensor, start_tokens=tf.fill([ARGS.batchSize], 100), end_token=200)
-    #   decoder = tfa.seq2seq.BasicDecoder(cell, sampler=inference_sampler)
-    #   decoder.initialize(inputTensor, initial_state=final_state)
+    go_token = -1.
+    go_tokens = tf.fill((1, tf.shape(targetTensor)[1], ARGS.numberOfInputCodes), go_token)
+    dec_input = tf.concat([go_tokens, targetTensor], axis=0)
 
     # maximum_iterations=41
-    final_outputs, final_state, _ = tfa.seq2seq.dynamic_decode(decoder=decoder, output_time_major=True, decoder_init_input=lstm_outputs,
+    final_outputs, final_state, _ = tfa.seq2seq.dynamic_decode(decoder=decoder, output_time_major=True, decoder_init_input=dec_input,
                                                                decoder_init_kwargs={"initial_state": lstm_states, "sequence_length":seqLen})
-    #
-    # with tf.variable_scope('decoder_cell', reuse=True):
-    #   # start_tokens = tf.tile(tf.constant([100], dtype=tf.int32), [ARGS.batchSize], name='start_tokens')
-    #   start_tokens = tf.fill([ARGS.batchSize], 100)
-    #   # start_tokens = tf.fill([ARGS.batchSize], 100)
-    #   embedding_layer = tf.keras.layers.Embedding(271, 273)
-    #
-    #   inference_sampler = tfa.seq2seq.GreedyEmbeddingSampler(embedding_layer)
-    #   # inference_sampler = tfa.seq2seq.GreedyEmbeddingSampler()
-    #   # inference_sampler.initialize(targetTensor, start_tokens=start_tokens, end_token=200)
-    #
-    #   decoder = tfa.seq2seq.BasicDecoder(cell, sampler=inference_sampler)
-    #   # decoder.initialize(targetTensor, initial_state=lstm_states)
-    #
-    #   final_outputs, final_state, _ = decoder(None, start_tokens=start_tokens, end_token=200, initial_state=lstm_states)
 
+    # with tf.variable_scope('decoder_cell', reuse=True):
+    #   start_tokens = tf.fill((tf.shape(targetTensor)[1], 1), go_token)
+    #   inference_sampler = tfa.seq2seq.sampler.InferenceSampler(
+    #     sample_fn=lambda outputs: outputs,
+    #     sample_shape=[271],
+    #     sample_dtype=tf.float32,
+    #     end_fn=lambda sample_ids: False)
+    #
+    #   inference_sampler.initialize(start_inputs=start_tokens)
+    #   inference_decoder = tfa.seq2seq.BasicDecoder(dec_cell, sampler=inference_sampler)
+    #
+    #   decoder_init_input = tf.fill((100, tf.shape(targetTensor)[-1] + tf.shape(lstm_states[-1].h)[-1]), 0.0)
+    #   final_outputs, final_state, _ = tfa.seq2seq.dynamic_decode(decoder=inference_decoder, output_time_major=True, decoder_init_input=decoder_init_input,
+    #                                                            decoder_init_kwargs={"initial_state": lstm_states})
 
     if ARGS.state == "cell":
       return final_state[-1].c #lstm_states has shape (c, h) where c are the cell states and h the hidden states
@@ -233,12 +227,11 @@ def build_model():
   with graph.as_default():
     xf = tf.placeholder(tf.float32, [None, None, ARGS.numberOfInputCodes], name="inputs")
     yf = tf.placeholder(tf.float32, [None, None, ARGS.numberOfInputCodes], name="labels")
+    df = tf.placeholder(tf.float32, [ARGS.numberOfInputCodes, ARGS.hiddenDimSize[-1]], name="vocab_size")
     maskf = tf.placeholder(tf.float32, [None, None], name="mask")
     seqLen = tf.placeholder(tf.float32, [None], name="nVisitsOfEachPatient_List")
-    training = tf.placeholder(tf.bool)
 
-    # flowingTensor = tf.cond(tf.less(x, y), lambda:EncoderDecoderBahdanau_layer(xf, yf, seqLen), lambda:xf)
-    flowingTensor = EncoderDecoderBahdanau_layer(xf, yf, seqLen)
+    flowingTensor = EncoderDecoderBahdanau_layer(xf, yf, df, seqLen)
     flowingTensor, weights = FC_layer(flowingTensor)
     flowingTensor = tf.math.multiply(flowingTensor, maskf[:,:,None], name="predictions")
 
@@ -247,7 +240,7 @@ def build_model():
     prediction_loss = tf.math.reduce_mean(tf.math.reduce_sum(cross_entropy, axis=[2, 0]) / seqLen)
     L2_regularized_loss = prediction_loss + tf.math.reduce_sum(ARGS.LregularizationAlpha * (weights ** 2))
 
-    optimizer = tf.train.AdadeltaOptimizer(learning_rate=0.5, rho=0.95, epsilon=1e-06).minimize(L2_regularized_loss)
+    optimizer = tf.train.AdadeltaOptimizer(learning_rate=ARGS.learningRate, rho=0.95, epsilon=1e-06).minimize(L2_regularized_loss)
 
     # Bahdanau (855)
     # global_step = tf.Variable(0, trainable=False)
@@ -264,14 +257,14 @@ def build_model():
     # learning_rate = tf.train.exponential_decay(1.0, global_step, 100, 0.9)
     # optimizer = tf.train.AdadeltaOptimizer(learning_rate, rho=0.95, epsilon=1e-06).minimize(L2_regularized_loss, global_step=global_step)
 
-    return tf.global_variables_initializer(), graph, optimizer, L2_regularized_loss, xf, yf, maskf, seqLen, training, flowingTensor
+    return tf.global_variables_initializer(), graph, optimizer, L2_regularized_loss, xf, yf, maskf, seqLen, flowingTensor
 
 def train_model():
   print("==> data loading")
   trainSet, testSet = load_data()
 
   print("==> model building")
-  init, graph, optimizer, loss, x, y, mask, seqLen, training, predictions = build_model()
+  init, graph, optimizer, loss, x, y, mask, seqLen, predictions = build_model()
 
   print ("==> training and validation")
   batchSize = ARGS.batchSize
@@ -304,14 +297,14 @@ def train_model():
         # end_token = np.full((yf.shape[0], yf.shape[1], 1), 200)
         # yf = np.concatenate([yf, end_token], axis=-1)
 
-        feed_dict = {x: xf, y: yf, mask: maskf, seqLen: nVisitsOfEachPatient_List, training: True}
+        feed_dict = {x: xf, y: yf, mask: maskf, seqLen: nVisitsOfEachPatient_List}
         _, trainCrossEntropy = sess.run([optimizer, loss], feed_dict=feed_dict)
 
         trainCrossEntropyVector.append(trainCrossEntropy)
         iteration += 1
 
       print('-> Epoch: %d, mean cross entropy considering %d TRAINING batches: %f' % (epoch_counter, n_batches, np.mean(trainCrossEntropyVector)))
-      nValidBatches, validationCrossEntropy = performEvaluation(sess, loss, x, y, mask, seqLen, training, testSet)
+      nValidBatches, validationCrossEntropy = performEvaluation(sess, loss, x, y, mask, seqLen, testSet)
       print('      mean cross entropy considering %d VALIDATION batches: %f' % (nValidBatches, validationCrossEntropy))
 
       if validationCrossEntropy < bestValidationCrossEntropy:
@@ -354,12 +347,13 @@ def parse_arguments():
   parser.add_argument('inputFileRadical', type=str, metavar='<visit_file>', help='File radical name (the software will look for .train and .test files) with pickled data organized as patient x admission x codes.')
   parser.add_argument('outFile', metavar='out_file', default='model_output', help='Any file directory to store the model.')
   parser.add_argument('--maxConsecutiveNonImprovements', type=int, default=10, help='Training wiil run until reaching the maximum number of epochs without improvement before stopping the training')
-  parser.add_argument('--hiddenDimSize', type=str, default='[271]', help='Number of layers and their size - for example [100,200] refers to two layers with 100 and 200 nodes.')
+  parser.add_argument('--hiddenDimSize', type=str, default='[1084]', help='Number of layers and their size - for example [100,200] refers to two layers with 100 and 200 nodes.')
   parser.add_argument('--state', type=str, default='cell', help='Pass cell or hidden to fully connected layer')
-  parser.add_argument('--attentionDimSize', type=int, default=3, help='Number of attention layer dense units')
+  # parser.add_argument('--attentionDimSize', type=int, default=3, help='Number of attention layer dense units')
   parser.add_argument('--batchSize', type=int, default=100, help='Batch size.')
   parser.add_argument('--nEpochs', type=int, default=1000, help='Number of training iterations.')
   parser.add_argument('--LregularizationAlpha', type=float, default=0.001, help='Alpha regularization for L2 normalization')
+  parser.add_argument('--learningRate', type=float, default=0.5, help='Learning rate.')
   parser.add_argument('--dropoutRate', type=float, default=0.45, help='Dropout probability.')
 
   ARGStemp = parser.parse_args()
