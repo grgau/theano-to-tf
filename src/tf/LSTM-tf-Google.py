124127d2b5e20c73985253f6bc81f81a1877c531
pedro.ferracini <pedro.ferracini@imusics.com>
2021-06-21 19:20:54 -0300

    Add GPU support

--- src/tf/LSTM-tf-Google.py
+++ src/tf/LSTM-tf-Google.py
@@ -129,16 +129,17 @@ def build_model():
     maskf = tf.placeholder(tf.float32, [None, None], name="mask")
     seqLen = tf.placeholder(tf.float32, [None], name="nVisitsOfEachPatient_List")
 
-    flowingTensor = DTLSTM_layer(xf, seqLen)
-    flowingTensor, weights = FC_layer(flowingTensor)
-    flowingTensor = tf.math.multiply(flowingTensor, maskf[:,:,None], name="predictions")
+    with tf.device('/gpu:0'):
+      flowingTensor = DTLSTM_layer(xf, seqLen)
+      flowingTensor, weights = FC_layer(flowingTensor)
+      flowingTensor = tf.math.multiply(flowingTensor, maskf[:,:,None], name="predictions")
 
-    epislon = 1e-8
-    cross_entropy = -(yf * tf.log(flowingTensor + epislon) + (1. - yf) * tf.log(1. - flowingTensor + epislon))
-    prediction_loss = tf.math.reduce_mean(tf.math.reduce_sum(cross_entropy, axis=[2, 0]) / seqLen)
-    L2_regularized_loss = prediction_loss + tf.math.reduce_sum(ARGS.LregularizationAlpha * (weights ** 2))
+      epislon = 1e-8
+      cross_entropy = -(yf * tf.log(flowingTensor + epislon) + (1. - yf) * tf.log(1. - flowingTensor + epislon))
+      prediction_loss = tf.math.reduce_mean(tf.math.reduce_sum(cross_entropy, axis=[2, 0]) / seqLen)
+      L2_regularized_loss = prediction_loss + tf.math.reduce_sum(ARGS.LregularizationAlpha * (weights ** 2))
 
-    optimizer = tf.train.AdadeltaOptimizer(learning_rate=ARGS.LearningRate, rho=0.95, epsilon=1e-06).minimize(L2_regularized_loss)
+      optimizer = tf.train.AdadeltaOptimizer(learning_rate=ARGS.LearningRate, rho=0.95, epsilon=1e-06).minimize(L2_regularized_loss)
 
     # global_step = tf.Variable(0, trainable=False)
     # learning_rate = tf.train.exponential_decay(1.0, global_step, 100, 0.87)
@@ -170,7 +171,7 @@ def train_model():
   iConsecutiveNonImprovements = 0
   epoch_counter = 0
 
-  with tf.Session(graph=graph) as sess:
+  with tf.Session(graph=graph, config=tf.ConfigProto(allow_soft_placement=True)) as sess:
     sess.run(init)
 
     for epoch_counter in range(ARGS.nEpochs):
