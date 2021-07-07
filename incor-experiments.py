4852c3f8ab7132fc240e87cefc2f6a12ab246bf7
pedro.ferracini <pedro.ferracini@imusics.com>
2021-07-06 21:02:32 -0300

    Add incor experiments for gru

--- incor-experiments.py
+++ incor-experiments.py
@@ -4,133 +4,133 @@ from contextlib import redirect_stdout
 
 for r in range(5):
   # cell [391]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[391] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[391] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[391]")
+      print(" --hiddenDimSize=[391]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [391, 391]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[391,391] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[391,391] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391,391]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391,391] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[391, 391]")
+      print(" --hiddenDimSize=[391, 391]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [391, 391, 391]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[391,391,391] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[391,391,391] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391,391,391]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[391,391,391] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[391, 391, 391]")
+      print(" --hiddenDimSize=[391, 391, 391]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [783]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[783] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[783] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[783]")
+      print(" --hiddenDimSize=[783]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [783, 783]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[783,783] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[783,783] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783,783]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783,783] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[783, 783]")
+      print(" --hiddenDimSize=[783, 783]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [783, 783, 783]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[783,783,783] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[783,783,783] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783,783,783]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[783,783,783] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[783, 783, 783]")
+      print(" --hiddenDimSize=[783, 783, 783]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [1566]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[1566] --maxConsecutiveNonImprovements=5',shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[1566] --maxConsecutiveNonImprovements=10',shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[1566]")
+      print(" --hiddenDimSize=[1566]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [1566, 1566]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[1566,1566] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[1566,1566] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566,1566]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566,1566] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[1566, 1566]")
+      print(" --hiddenDimSize=[1566, 1566]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [1566, 1566, 1566]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[1566,1566,1566] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[1566,1566,1566] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566,1566,1566]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[1566,1566,1566] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[1566, 1566, 1566]")
+      print(" --hiddenDimSize=[1566, 1566, 1566]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [3133]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[3133] --maxConsecutiveNonImprovements=5',shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[3133] --maxConsecutiveNonImprovements=10',shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[3133]")
+      print(" --hiddenDimSize=[3133]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [3133, 3133]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[3133,3133] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[3133,3133] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133,3133]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133,3133] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[3133, 3133]")
+      print(" --hiddenDimSize=[3133, 3133]")
       print(stdout.decode().split("\n"))
       print("\n")
 
   # cell [3133, 3133, 3133]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "data/incor/_3133" compiled_models/incor-lstm-model --state=cell --hiddenDimSize=[3133,3133,3133] --maxConsecutiveNonImprovements=5', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "src/data-incor/incor_90-10_3133" compiled_models/incor-gru-model  --hiddenDimSize=[3133,3133,3133] --maxConsecutiveNonImprovements=10', shell=True)
   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "data/incor/_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133,3133,3133]', shell=True)
+  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "src/data-incor/incor_90-10_3133"' + " \"" + best_model + "\"" + ' --hiddenDimSize=[3133,3133,3133] --runName=A-INCOR_', shell=True)
 
-  with open(str(r) + 'lstm-incor-experiments.txt', 'a') as f:
+  with open(str(r) + '_gru-incor-experiments.txt', 'a') as f:
     with redirect_stdout(f):
-      print("--state=cell --hiddenDimSize=[3133, 3133, 3133]")
+      print(" --hiddenDimSize=[3133, 3133, 3133]")
       print(stdout.decode().split("\n"))
       print("\n")
