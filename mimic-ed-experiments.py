0a67c1913616baa4d3db88c5346f9e297e911ec7
pedro.ferracini <pedro.ferracini@imusics.com>
2021-09-15 11:07:50 -0300

    Update tests

--- mimic-ed-experiments.py
+++ mimic-ed-experiments.py
@@ -74,41 +74,41 @@ for r in range(3):
       print(stdout.decode().split("\n"))
       print("\n")
 
-for r in range(3):
-  # cell [1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=5 --hiddenDimSize=[1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=5 --hiddenDimSize=[1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=5 --hiddenDimSize=[1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=5 --hiddenDimSize=[1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084,1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=5 --hiddenDimSize=[1084,1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=5 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=5 --hiddenDimSize=[1084, 1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=5 --hiddenDimSize=[1084, 1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
 
 for r in range(3):
@@ -183,41 +183,41 @@ for r in range(3):
       print(stdout.decode().split("\n"))
       print("\n")
 
-for r in range(3):
-  # cell [1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=10 --hiddenDimSize=[1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=10 --hiddenDimSize=[1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=10 --hiddenDimSize=[1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=10 --hiddenDimSize=[1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084,1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=10 --hiddenDimSize=[1084,1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=10 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=10 --hiddenDimSize=[1084, 1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=10 --hiddenDimSize=[1084, 1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
 
 for r in range(3):
@@ -292,38 +292,38 @@ for r in range(3):
       print(stdout.decode().split("\n"))
       print("\n")
 
-for r in range(3):
-  # cell [1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=15 --hiddenDimSize=[1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=15 --hiddenDimSize=[1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=15 --hiddenDimSize=[1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=15 --hiddenDimSize=[1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
 
-for r in range(3):
-  # cell [1084, 1084, 1084]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084,1084,1084]', shell=True)
-  best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
-  stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
+# for r in range(3):
+#   # cell [1084, 1084, 1084]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722" compiled_models/9722-encdec-model --attentionDimSize=15 --hiddenDimSize=[1084,1084,1084]', shell=True)
+#   best_model = stdout.decode().split("\n")[-5].split(" ")[-1]
+#   stdout = subprocess.check_output('python3.7 src/tf/LSTM-tf-Google-test.py "../clinical-prediction/data/mimic-ed/both/mimic-ed_90-10_9722"' + " \"" + best_model + "\"" + ' --attentionDimSize=15 --hiddenDimSize=[1084,1084,1084] --runName=Att_c-MIMIC-9722_', shell=True)
 
-  with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
-    with redirect_stdout(f):
-      print("--attentionDimSize=15 --hiddenDimSize=[1084, 1084, 1084]")
-      print(stdout.decode().split("\n"))
-      print("\n")
+#   with open(str(r) + '_encdec-mimic-9722-experiments.txt', 'a') as f:
+#     with redirect_stdout(f):
+#       print("--attentionDimSize=15 --hiddenDimSize=[1084, 1084, 1084]")
+#       print(stdout.decode().split("\n"))
+#       print("\n")
